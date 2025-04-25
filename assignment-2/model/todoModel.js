57a72adcec1e185360074f49eb3c115c47e4b352
gwagjiug <kwjo0228@naver.com>
2025-04-25 22:14:43 +0900

    feat: 할 일 저장소 초기화 및 조회 기능 추가

--- assignment-2/model/todoModel.js
+++ assignment-2/model/todoModel.js
@@ -16,7 +16,30 @@ class TodoDTO {
   }
 }
 
+const STORAGE_KEY = 'todos';
+
+/**
+ * 초기 데이터가 없을 경우 샘플 데이터로 초기화합니다.
+ * @param {Array} sampleTodos - 샘플 할 일 목록 배열
+ */
+const initTodoStorage = (sampleTodos) => {
+  if (!localStorage.getItem(STORAGE_KEY)) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(sampleTodos || []));
+  }
+};
+
+/**
+ * 저장된 모든 할 일을 조회합니다.
+ * @returns {Array} TodoDTO 객체의 배열
+ */
+const getAllTodos = () => {
+  const todos = localStorage.getItem(STORAGE_KEY);
+  return todos ? JSON.parse(todos).map(todo => new TodoDTO(todo)) : [];
+};
+
 
 export {
   TodoDTO,
+  initTodoStorage,
+  getAllTodos,
 };
