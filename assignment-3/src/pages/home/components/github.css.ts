9fddccb8a415e58ce0abd37638e43df6e13be69b
gwagjiug <kwjo0228@naver.com>
2025-05-02 23:03:15 +0900

    fix: GitHub 컨테이너 높이를 80svh로 수정

--- assignment-3/src/pages/home/components/github.css.ts
+++ assignment-3/src/pages/home/components/github.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 
 export const gihubContainer = style({
   width: "100%",
-  height: "100svh",
+  height: "80svh",
   display: "flex",
   justifyContent: "center",
   flexDirection: "column",
