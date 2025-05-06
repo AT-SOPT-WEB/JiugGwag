d6afcd0113179739fe735cf7ea3b3d7ec2a5a766
gwagjiug <kwjo0228@naver.com>
2025-05-06 20:35:03 +0900

    feat: 메시지 컴포넌트의 메시지 유형 추가 및 내용 표시 로직 개선

--- assignment-3/src/components/message/message.tsx
+++ assignment-3/src/components/message/message.tsx
@@ -1,9 +1,26 @@
 interface MessageProps {
-  message: string;
+  message: "warning" | "correct" | "wrong";
+  result?: string;
 }
 
-const Message = ({ message }: MessageProps) => {
-  return <div>{message}</div>;
+const Message = ({ message, result }: MessageProps) => {
+  let content;
+
+  switch (message) {
+    case "warning":
+      content = "서로 다른 숫자 3자리를 입력해주세요!";
+      break;
+    case "correct":
+      content = "게임 승리!";
+      break;
+    case "wrong":
+      content = result;
+      break;
+    default:
+      content = "";
+  }
+
+  return <div>{content}</div>;
 };
 
 export default Message;
--- assignment-3/src/pages/home/components/baseball-game.tsx
+++ assignment-3/src/pages/home/components/baseball-game.tsx
@@ -15,7 +15,7 @@ const BaseBallGame = () => {
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
-    if (/^\d*$/.test(input) && input.length <= 3) {
+    if (/^\d*$/.test(input)) {
       setNumber(input);
     }
   };
