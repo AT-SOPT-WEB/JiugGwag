f5a55d4955264ca2862e7ddca565a1c4ac0fb2e4
gwagjiug <kwjo0228@naver.com>
2025-05-06 20:13:44 +0900

    feat: 검색 바 컴포넌트를 추가하고 숫자 입력 기능 구현

--- assignment-3/src/pages/home/components/baseball-game.tsx
+++ assignment-3/src/pages/home/components/baseball-game.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import SearchBar from "../../../components/searchBar/search-bar";
+import * as style from "./github.css";
+
 const BaseBallGame = () => {
-  return <div></div>;
+  const [number, setNumber] = useState("");
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(e.target.value);
+  };
+
+  return (
+    <section className={style.gihubContainer}>
+      <div>
+        <SearchBar
+          placeholder="3자리 숫자를 입력해주세요"
+          value={number}
+          onChange={handleOnChange}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default BaseBallGame;
