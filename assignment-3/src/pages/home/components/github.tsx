02fd5d85e59e1c9b9577eb99bbb271badf3ce733
gwagjiug <kwjo0228@naver.com>
2025-05-06 22:58:00 +0900

    feat: GitHub 데이터 로직을 훅으로 분리하여 코드 구조 개선

--- assignment-3/src/pages/home/components/github.tsx
+++ assignment-3/src/pages/home/components/github.tsx
@@ -1,60 +1,24 @@
-import { useState } from "react";
-import { useGithubData } from "../hooks/use-github-data";
-import {
-  getRecentUsers,
-  removeRecentUser,
-  saveRecentUser,
-} from "../../../utils/local-storage";
+import { useGithubData } from "../hooks/get-github-data";
+import { useGithubLogic } from "../hooks/use-github-logic";
 import SearchBar from "../../../components/searchBar/search-bar";
 import * as styles from "./page.css";
 import Chip from "../../../components/chip/chip";
 import Card from "../../../components/card/card";
 
 const Github = () => {
-  const [recentUsers, setRecentUsers] = useState<string[]>([]);
-  const [user, setUser] = useState("");
-  const [queryUser, setQueryUser] = useState("");
-  const [isCardVisible, setIsCardVisible] = useState(false);
+  const {
+    user,
+    recentUsers,
+    queryUser,
+    isCardVisible,
+    handleOnChange,
+    handleKeyDown,
+    handleCloseCard,
+    handleDeleteChip,
+    handleChipClick,
+  } = useGithubLogic();
 
   const { data, isLoading, error } = useGithubData(queryUser);
-
-  const validateInput = (input: string) => input.trim().length > 0;
-
-  const updateQueryUser = (trimmedUser: string) => {
-    setQueryUser(trimmedUser);
-    setIsCardVisible(true);
-  };
-
-  const updateRecentUsers = (trimmedUser: string) => {
-    saveRecentUser(trimmedUser);
-    setRecentUsers(getRecentUsers());
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && validateInput(user)) {
-      const trimmedUser = user.trim();
-      updateQueryUser(trimmedUser);
-      updateRecentUsers(trimmedUser);
-    }
-  };
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser(e.target.value);
-  };
-
-  const handleCloseCard = () => {
-    setIsCardVisible(false);
-    setUser("");
-  };
-
-  const handleDeleteChip = (keyword: string) => {
-    removeRecentUser(keyword);
-    setRecentUsers(getRecentUsers());
-  };
-
-  const handleChipClick = (keyword: string) => {
-    setQueryUser(keyword);
-  };
 
   return (
     <>
